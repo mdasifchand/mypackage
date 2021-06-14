72347c4f59458aefed4c0f649a4fe8a9f0359af8
light <mdasifchand@gmail.com>
2021-06-15 01:15:42 +0200

    Update publishersubscriber.cpp

--- mypackage/src/publishersubscriber.cpp
+++ mypackage/src/publishersubscriber.cpp
@@ -1,11 +1,9 @@
 //
-// Created by ethernet on 24.05.18.
+// Created by Asif
 //
 
 
 #include "publishersubscriber.h"
-
-
 
 
 camera::PublishSubscribeCamera::PublishSubscribeCamera(): _it(_nh){   //const references are to be initialized in initialization list ? check it out
@@ -14,15 +12,11 @@ camera::PublishSubscribeCamera::PublishSubscribeCamera(): _it(_nh){   //const re
     // images are directed to the call back method whenever an image appears
     //call back method performs necessary operations like undistorting etc.,.
 
-
-
     _imagesub = _it.subscribe(subscribetopic, 1 ,&PublishSubscribeCamera::callback, this);
-
 
     _imagepub = _it.advertise(publishtopic, 1);
 
     //cv::namedWindow (OPENCV_WINDOW);
-
 
 }
 
@@ -31,32 +25,22 @@ camera::PublishSubscribeCamera::~PublishSubscribeCamera() {
 
     //kill any open opencv windows
     //release unwanted memory
-
-
 }
 
-
 void camera::PublishSubscribeCamera::callback(const sensor_msgs::ImageConstPtr& msg){
-
-
 
     cv_bridge::CvImagePtr cv_ptr; // converts sensor to cvbridge
 
     try
     {
-
         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-
-
-
 
     }
     catch (cv_bridge::Exception& e)
 
     {
-
         ROS_ERROR("cv_bridge exception: %s", e.what());
-
+        
         return;
     }
 
